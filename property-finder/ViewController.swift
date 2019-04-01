ab2756fec3ffc6747b04b20a5cf06c4978c84dc2
Madson C <madson@cardoso.org>
2019-04-01 04:00:35 -0700

    Implements ListAdapterDataSource on ViewController.

--- property-finder/ViewController.swift
+++ property-finder/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import IGListKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet var collectionView: UICollectionView!
+    private var page = 1
+    private let searchClient = SearchClient()
+
+    var properties: [Property] = []
+    
+    lazy var adapter: ListAdapter = {
+        return ListAdapter(updater: ListAdapterUpdater(), viewController: self, workingRangeSize: 0)
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+                
+        fetchProperties()
+        
+        adapter.collectionView = collectionView
+        adapter.dataSource = self
     }
 
-
+    func fetchProperties(for query: String? = nil) {
+        searchClient.properties(for: self.page) { (properties) in
+            self.properties += properties
+            self.adapter.performUpdates(animated: true)
+        }
+    }
 }
 
+extension ViewController: ListAdapterDataSource {
+    
+    func objects(for listAdapter: ListAdapter) -> [ListDiffable] {
+        return self.properties
+    }
+    
+    func listAdapter(_ listAdapter: ListAdapter, sectionControllerFor object: Any) -> ListSectionController {
+        let sectionController = PropertySectionController()
+        return sectionController
+    }
+    
+    func emptyView(for listAdapter: ListAdapter) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        return view
+    }
+}
