6e50843ef8389505d6fcb244b7889a76625a032c
Madson C <madson@cardoso.org>
2019-04-01 05:27:55 -0700

    Implement sort feature on ViewController.

--- property-finder/ViewController.swift
+++ property-finder/ViewController.swift
@@ -24,19 +24,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.contentInset = UIEdgeInsets(top: 15, left: 0, bottom: 15, right: 0)
                 
-        fetchProperties()
+        fetchProperties(for: self.page, sort: nil)
         
         adapter.collectionView = collectionView
         adapter.dataSource = self
     }
 
-    func fetchProperties(for query: String? = nil) {
+    func fetchProperties(for page: Int, sort: SortOption?) {
         DataRequest.addAcceptableImageContentTypes(["image/jpeg", "image/jpg"])
 
-        searchClient.properties(for: self.page) { (properties) in
+        searchClient.properties(for: self.page, sort: sort) { (properties) in
             self.properties += properties
             self.adapter.performUpdates(animated: true)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let viewController = segue.destination as? SortViewController {
+            viewController.delegate = self
         }
     }
 }
@@ -56,5 +63,14 @@ extension ViewController: ListAdapterDataSource {
         let view = UIView()
         view.backgroundColor = .lightGray
         return view
+    }
+}
+
+extension ViewController: SortViewControllerDelegate {
+    func didSelectSortOption(sort: SortOption) {
+        self.page = 1
+        self.properties = []
+        self.adapter.performUpdates(animated: true)
+        fetchProperties(for: self.page, sort: sort)
     }
 }
